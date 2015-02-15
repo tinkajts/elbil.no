50e708de28e701f095745375198840b82c91f076
tinkajts@gmail.com <tinkajts@gmail.com>
2015-02-15 19:27:03 +0100

    orange pil i blogg-visning

--- elbil.no/templates/elbil_no/html/com_content/category/blog_item.php
+++ elbil.no/templates/elbil_no/html/com_content/category/blog_item.php
@@ -32,7 +32,7 @@ JHtml::_('behavior.tooltip');
 					echo 'class="caption"'.' title="' .htmlspecialchars($images->image_intro_caption) .'"';
 				endif; ?>
 				src="<?php echo htmlspecialchars($images->image_intro); ?>" alt="<?php echo htmlspecialchars($images->image_intro_alt); ?>"/>
-			<img src="/images/arrow-for-blog-pictures.png" class="gronnpil" width="88px" height="66px"/>
+			<img src="/images/orangepilboks.png" class="gronnpil" width="42px" height="30px"/>
 		</a>
 		
 	</div>
